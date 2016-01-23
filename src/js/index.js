add545d85d9f67f1b842d00cc1fdf1e9296657e3
Jesús Otero <jesusoterogomez@gmail.com>
2016-01-24 00:08:39 +0100

    Updated src scripts

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,3 @@
-let test = "test";
+let test = "This website is using ES6 Language";
 
 console.log(test);
--- src/js/test.js
+++ src/js/test.js
@@ -1,3 +1,5 @@
-let a = "test";
+let a = "You can add any other file in the /js directory";
+let b = "And it will be merged automatically during the build process";
 
 console.log(a);
+console.log(b);
